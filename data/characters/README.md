74372824088b391c486c959a45ba8c37d5ea439e
Riccardo Carlesso <palladiusbonton@gmail.com>
2026-04-20 08:38:16 +0200

    🚀 fix: use checked-in yukihiro character for tests -- made with Gemini CLI

---
+++ data/characters/README.md
@@ -0,0 +1,10 @@
+Two characters are aveialbel to use:
+- Yukihiro
+- Zenzile
+
+Add your own here taking spunto from these examples!
+
+1. Add a folder named YOUR_CHARACTER_NICK
+2. Add a few pics.
+3. copy the Character.yaml and update it to your needs.
+4. check it with "arnectl characters list"
--- spec/arneis/character_spec.rb
+++ spec/arneis/character_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe Arneis::Character do
     it 'returns all characters from data/characters' do
       chars = described_class.all
       expect(chars).to be_an(Array)
-      expect(chars.any? { |c| c.id == 'riccardo' }).to be true
+      expect(chars.any? { |c| c.id == 'yukihiro' }).to be true
     end
   end
 
   describe '#full_name' do
     it 'joins name and surname' do
-      char = described_class.new(File.expand_path('../../data/characters/riccardo/character.yaml', __dir__))
-      expect(char.full_name).to include('Riccardo')
+      # Using yukihiro since riccardo is not checked in
+      char = described_class.new(File.expand_path('../../data/characters/yukihiro/character.yaml', __dir__))
+      expect(char.full_name).to include('Yukihiro')
     end
   end
 end
